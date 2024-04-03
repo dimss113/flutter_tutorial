66127e9e908df60ab1b3c91574120651aebffc7b
Dimas Fadilah Akbar <5025211010@mhs.its.ac.id>
2024-04-03 12:37:36 +0700

    feat: implement map on string arrays

--- lib/main.dart
+++ lib/main.dart
@@ -5,138 +5,45 @@ import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: NinjaCard(),
-  ));  
+    home: Quote(),
+  ));
 }
 
 // ignore: camel_case_types
-class NinjaCard extends StatefulWidget {
-  const NinjaCard({super.key});
+class Quote extends StatefulWidget {
+  const Quote({super.key});
 
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  // ignore: library_private_types_in_public_api
+  _QuoteState createState() => _QuoteState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
-  int ninjaLevel = 0;
+// ignore: camel_case_types
+class _QuoteState extends State<Quote> {
+
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken.',
+    'So many books, so little time.',
+    'A room without books is like a body without a soul.',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    backgroundColor: Colors.brown[900],
-    appBar: AppBar(
-      title: const Text('Ninja ID Card'),
-      centerTitle: true,
-      backgroundColor: Colors.brown[600],
-      elevation: 0.0,
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        setState(() {
-          ninjaLevel += 1;
-        });
-      },
-      backgroundColor: Colors.grey[600],
-      child: const Icon(Icons.add),
-    ),
-    body:  Padding(
-      padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Center(
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/pict_1.jpg'),
-              radius: 40.0,
-            ),
-          ),
-          const Divider(
-            height: 90.0,
-            color: Colors.grey,
-          ),
-          const Text(
-            'NAME',
-            style: TextStyle(
-              color: Colors.grey,
-              letterSpacing: 2.0,
-            ),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          const Text(
-            'Killer Bee',
-            style: TextStyle(
-              color: Color.fromARGB(255, 136, 126, 92),
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          const Text(
-            'CURRENT NINJA LEVEL',
-            style: TextStyle(
-              color: Colors.grey,
-              letterSpacing: 2.0,
-            ),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            '$ninjaLevel',
-            style:const  TextStyle(
-              color: Color.fromARGB(255, 136, 126, 92),
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          const Row(
-            children: <Widget>[
-              Icon(
-                Icons.email,
-                color: Color.fromARGB(255, 194, 184, 184),
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Text(
-                'dimasfadilah20@gmail.com',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 194, 184, 184),
-                  fontSize: 18.0,
-                  letterSpacing: 1.0,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    )
-  );
-  }
-}
-
-class Test extends StatefulWidget {
-  const Test({super.key});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: const Text('Quote'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+          children: <Widget>[
+            Column(
+              children: quotes.map((quote) {
+                return Text(quote);
+              }).toList(), // Display the third quote
+            )
+          ],
+        ));
   }
 }
