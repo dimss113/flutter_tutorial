900c3100da5d7ce9862d23df46d421e35f0fef24
Dimas Fadilah Akbar <5025211010@mhs.its.ac.id>
2024-04-03 12:52:20 +0700

    feat: add card

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,63 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'quote.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Quote(),
+    home: QuoteList(),
   ));
 }
 
 // ignore: camel_case_types
-class Quote extends StatefulWidget {
-  const Quote({super.key});
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
 
   @override
   // ignore: library_private_types_in_public_api
-  _QuoteState createState() => _QuoteState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
 // ignore: camel_case_types
-class _QuoteState extends State<Quote> {
-
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken.',
-    'So many books, so little time.',
-    'A room without books is like a body without a soul.',
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        quote: 'Be yourself; everyone else is already taken.',
+        author: 'Oscar Wilde'),
+    Quote(quote: 'So many books, so little time.', author: 'Frank Zappa'),
+    Quote(
+        quote: 'A room without books is like a body without a soul.',
+        author: 'Marcus Tullius Cicero'),
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.quote,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              )
+            ),
+            const SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +73,7 @@ class _QuoteState extends State<Quote> {
           children: <Widget>[
             Column(
               children: quotes.map((quote) {
-                return Text(quote);
+                return quoteTemplate(quote); // Display the first and second quote
               }).toList(), // Display the third quote
             )
           ],
---
+++ lib/quote.dart
@@ -0,0 +1,9 @@
+class Quote {
+  final String quote;
+  final String author;
+
+  Quote({required this.quote, required this.author});
+}
+
+
+Quote quote = Quote(quote: 'Be yourself; everyone else is already taken.', author: 'Oscar Wilde');
